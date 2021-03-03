6a833da4b225072ca6883189c24d518cd27752e0
AntonLPN <68461689+AntonLPN@users.noreply.github.com>
2021-03-03 11:12:02 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using myDLL;
+using System.IO;
+
+
+
+//Задание 1.
+//Создайте собственную библиотеку классов для выполнения следующих операций:
+//1.Создание файла
+//2.Запись в файл
+//3.	Считывание из файла
+//Предусмотрите возможность возникновения исключительной ситуации. Используйте библиотеку для работы с программой «Записная книжка»
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int menu = 0;
+            myDLL.Class1 test = new Class1();
+            test.Create_directory();//создаем папку с названием TEST_FOLDER
+
+            do
+            {
+                Console.WriteLine("1-Создать файл");
+                Console.WriteLine("2-Записать в файл");
+                Console.WriteLine("3-Считать с файла");
+                Console.WriteLine("4-Показать название файлов в папке и путь к ним");
+                
+                Console.Write(">>");
+
+                menu = int.Parse(Console.ReadLine());
+
+                switch (menu)
+                {
+                    case 1:
+                        {
+                            Console.Write("Введите название файла >>");
+                            string name_file = Console.ReadLine();
+                            test.Creat_file_txt(name_file);
+                         
+
+                        }
+                        break;
+
+
+                    case 2:
+                        {
+                            Console.Write("Введите название фала без указания формата txt :>>");
+                            string name_file = Console.ReadLine();
+                            test.Write_to_the_file_txt(name_file);
+
+                        }
+                        break;
+
+
+                    case 3:
+                        {
+                            Console.Write("Введите название файла без указания формата : >>");
+                            string name_file = Console.ReadLine();
+                            try
+                            {
+                                Console.WriteLine(test.ReadFile(name_file));
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex.Message);
+                               
+                            }
+                           
+                        }break;
+                    case 4:
+                        {
+                            foreach (string item in test.all_file_txt())
+                            {
+                                Console.WriteLine(item);
+                                using (StreamReader stream = File.OpenText(item))
+                                {
+
+                                    Console.WriteLine(stream.ReadToEnd());
+                                }
+                            }
+                        }
+                        break;
+
+
+
+                    default:
+                        break;
+                }
+
+
+            } while (menu!=0);
+
+
+        }
+    }
+}
---
+++ myDLL/Class1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+//Задание 1.
+//Создайте собственную библиотеку классов для выполнения следующих операций:
+//1.Создание файла
+//2.Запись в файл
+//3.	Считывание из файла
+//Предусмотрите возможность возникновения исключительной ситуации. Используйте библиотеку для работы с программой «Записная книжка»
+
+namespace myDLL
+{
+    public class Class1
+    {
+        /// <summary>
+        /// Создает папку с названием TEST_FOLDER
+        /// </summary>
+        /// <returns> string name_directory </returns>
+        public string Create_directory()
+        {
+            string name_directory = "TEST_FOLDER";
+            DirectoryInfo directory = new DirectoryInfo(name_directory);
+            name_directory = @"TEST_FOLDER\";
+            directory.Create();
+            //возвращает путь с именем папки
+            return name_directory;
+        }
+
+        /// <summary>
+        /// Создает файл txt в папке TEST_FOLDER
+        /// </summary>
+        /// <param name="name_file_without_format"></param>
+        public void Creat_file_txt(string name)
+        {
+            string dir = Path.Combine(this.Create_directory(),name+".txt");//аргументы путь к папке и название создаваемого файла с указанием расширения
+         
+            FileStream filestream = new FileStream(dir, FileMode.Create);
+            filestream.Close();
+        }
+
+        /// <summary>
+        /// Метод получения в массив перечня всех файлов txt хранящихся в папке TEST_FOLDER
+        /// </summary>
+        /// <returns> string [] </returns>
+        public string [] all_file_txt()
+        {
+
+            //заносим названия всех файлов в массив строк
+            string[] all_recordings = Directory.GetFiles(this.Create_directory(), "*.txt");//добавляем патерн txt для поиска только текстовых файлов
+
+            return all_recordings;
+        }
+
+        /// <summary>
+        /// Открывает блокнот для вввода напрямую текста
+        /// </summary>
+        /// <param name="name_file_without_format"></param>
+        public void Write_to_the_file_txt(string name_file)
+        {
+            System.Diagnostics.Process Proc = new System.Diagnostics.Process();
+            Proc.StartInfo.FileName = this.Create_directory()+name_file+".txt";
+
+            Proc.Start();
+            Proc.Close();
+        }
+
+        /// <summary>
+        /// Считывает информацию из файла в строку 
+        /// </summary>
+        /// <param name="name_file_without_format"></param>
+        /// <returns>StremReader</returns>
+        public string ReadFile(string name_file)
+        {
+
+            using (StreamReader sr = File.OpenText(this.Create_directory()+name_file+".txt"))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+
+    }
+}
